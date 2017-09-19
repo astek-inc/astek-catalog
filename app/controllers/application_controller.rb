ed82093ed740f30bfc094f8a2c2e55def602a65e
Edwin Horneij <edwin@astekwallcovering.com>
2017-09-19 10:15:12 -0700

    Use token authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::API
-  include ActionController::HttpAuthentication::Token
+
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :authenticate
+
+  private
+
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      # puts token
+      # puts options
+      token == ENV['SECRET_TOKEN']
+    end
+  end
+
 end
