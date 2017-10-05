3b981d273f08b0fdc90676be38b33a8bafda13fb
Edwin Horneij <edwin@astekwallcovering.com>
2017-10-05 09:31:55 -0700

    Update token authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,32 @@ class ApplicationController < ActionController::API
   private
 
   def authenticate
+    # puts 'xxx'
     authenticate_or_request_with_http_token do |token, options|
       # puts token
+      # puts ENV['SECRET_TOKEN']
       # puts options
       token == ENV['SECRET_TOKEN']
     end
+
   end
+
+  # def authenticate
+  #   puts 'xxx'
+  #   authenticate_token # || render_unauthorized
+  # end
+  #
+  # def authenticate_token
+  #   authenticate_with_http_token do |token, options|
+  #     puts token
+  #     token == ENV['SECRET_TOKEN']
+  #   end
+  # end
+  #
+  # # def render_unauthorized
+  # #   self.headers['WWW-Authenticate'] = 'Token realm="Application"'
+  # #   render json: 'Bad credentials', status: 401
+  # # end
 
   def not_found error
     render json: { error: error.message }, status: :not_found
