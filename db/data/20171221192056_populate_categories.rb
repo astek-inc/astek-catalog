61347048fc46a0e76c1d0530912f5cd1d83b4cb1
Edwin Horneij <edwin@astekwallcovering.com>
2018-06-18 15:49:40 -0700

    Change sites to websites, update domain names

--- db/data/20171221192056_populate_categories.rb
+++ db/data/20171221192056_populate_categories.rb
@@ -1,22 +1,22 @@
 class PopulateCategories < ActiveRecord::Migration
 
   CATEGORIES = [
-      { name: 'Contact Paper', description: '', keywords: '', sites: ['designyourwall.com'] },
-      { name: 'Decals', description: '', keywords: '', sites: ['designyourwall.com'] },
-      { name: 'Designers', description: '', keywords: '', sites: ['designyourwall.com'] },
-      { name: 'Digital Collections', description: '', keywords: '', sites: ['astekwallcovering.com', 'designyourwall.com'] },
-      { name: 'Naturals', description: '', keywords: '', sites: ['astekwallcovering.com', 'designyourwall.com'] },
-      { name: 'Residential', description: '', keywords: '', sites: ['astekwallcovering.com', 'designyourwall.com'] },
-      { name: 'Specialty', description: '', keywords: '', sites: ['astekwallcovering.com', 'designyourwall.com'] },
-      { name: 'Window Film', description: '', keywords: '', sites: ['designyourwall.com'] }
+      { name: 'Contact Paper', description: '', keywords: '', websites: ['astekhome.com'] },
+      { name: 'Decals', description: '', keywords: '', websites: ['astekhome.com'] },
+      { name: 'Designers', description: '', keywords: '', websites: ['astekhome.com'] },
+      { name: 'Digital Collections', description: '', keywords: '', websites: ['astek.com', 'astekhome.com'] },
+      { name: 'Naturals', description: '', keywords: '', websites: ['astek.com', 'astekhome.com'] },
+      { name: 'Residential', description: '', keywords: '', websites: ['astek.com', 'astekhome.com'] },
+      { name: 'Specialty', description: '', keywords: '', websites: ['astek.com', 'astekhome.com'] },
+      { name: 'Window Film', description: '', keywords: '', websites: ['astekhome.com'] }
   ]
 
   def self.up
     CATEGORIES.each do |cat|
-      sites = cat.delete(:sites)
+      websites = cat.delete(:websites)
       category = Category.create!(cat)
-      sites.each do |w|
-        category.sites << Site.find_by(domain: w)
+      websites.each do |w|
+        category.websites << Website.find_by(domain: w)
       end
     end
   end
