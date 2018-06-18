4bf3f39e27374d9ae48a3bda9da26606ba719226
Edwin Horneij <edwin@astekwallcovering.com>
2018-06-18 15:49:40 -0700

    Change sites to websites, update domain names

--- db/migrate/20171215223017_create_join_table_collections_sites.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableCollectionsSites < ActiveRecord::Migration
-  def change
-    create_join_table :collections, :sites do |t|
-      t.index [:collection_id, :site_id]
-      t.index [:site_id, :collection_id]
-    end
-  end
-end
