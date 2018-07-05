c7df60ad058a95f517a7ecc93526705e08bba9aa
Edwin Horneij <edwin@astekwallcovering.com>
2018-07-05 09:52:32 -0700

    Multiple variant images

--- db/data/20180226175950_populate_variants.rb
+++ db/data/20180226175950_populate_variants.rb
@@ -4,25 +4,25 @@ class PopulateVariants < ActiveRecord::Migration
       {
           design: 'Lumen',
           variants:[
-            {name: 'Lumen - Ash', sku: 'AD367-1', image_url: 'https://s3.us-west-2.amazonaws.com/astek-product-images/Lumen_Ash_web.jpg', colors: ['Red', 'Black and White']},
-            {name: 'Lumen - Nova', sku: 'AD367-2', image_url: 'https://s3.us-west-2.amazonaws.com/astek-product-images/Lumen_Nova_web.jpg', colors: ['Green', 'Black and White']},
-            {name: 'Lumen - Quasar', sku: 'AD367-3', image_url: 'https://s3.us-west-2.amazonaws.com/astek-product-images/Lumen_Quasar_web.jpg', colors: ['Indigo', 'Black and White']},
+            {name: 'Lumen - Ash', sku: 'AD367-1', image_urls: ['https://s3.us-west-2.amazonaws.com/astek-product-images/Lumen_Ash_web.jpg', 'https://s3-us-west-2.amazonaws.com/astek-catalog-tmp/Lumen_Ash_2.jpg'], colors: ['Red', 'Black and White']},
+            {name: 'Lumen - Nova', sku: 'AD367-2', image_urls: ['https://s3.us-west-2.amazonaws.com/astek-product-images/Lumen_Nova_web.jpg'], colors: ['Green', 'Black and White']},
+            {name: 'Lumen - Quasar', sku: 'AD367-3', image_urls: ['https://s3.us-west-2.amazonaws.com/astek-product-images/Lumen_Quasar_web.jpg'], colors: ['Indigo', 'Black and White']},
           ]
       },
       {
           design: 'Apparition',
           variants:[
-              {name: 'Apparition - Ethereal', sku: 'AD365-2', image_url: 'http://astek.s3.amazonaws.com/styles/extra-large/s3/Apparition_Ethereal2.jpg', colors: ['Orange', 'Black and White']},
-              {name: 'Apparition - Figment', sku: 'AD365-3', image_url: 'http://astek.s3.amazonaws.com/styles/extra-large/s3/Apparition_Figment2.jpg', colors: ['Blue', 'Black and White']},
-              {name: 'Apparition - Cumulus', sku: 'AD365-1', image_url: 'http://www.astekwallcovering.com/sites/default/files/styles/extra-large/public/designs/Apparition_Cumulus2.jpg', colors: ['Indigo', 'Black and White']},
+              {name: 'Apparition - Ethereal', sku: 'AD365-2', image_urls: ['http://astek.s3.amazonaws.com/styles/extra-large/s3/Apparition_Ethereal2.jpg'], colors: ['Orange', 'Black and White']},
+              {name: 'Apparition - Figment', sku: 'AD365-3', image_urls: ['http://astek.s3.amazonaws.com/styles/extra-large/s3/Apparition_Figment2.jpg'], colors: ['Blue', 'Black and White']},
+              {name: 'Apparition - Cumulus', sku: 'AD365-1', image_urls: ['http://www.astekwallcovering.com/sites/default/files/styles/extra-large/public/designs/Apparition_Cumulus2.jpg'], colors: ['Indigo', 'Black and White']},
           ]
       },
       {
         design: 'Xylem',
         variants:[
-            {name: 'Xylem - Chloroplast', sku: 'AD372-1', image_url: 'https://s3.amazonaws.com/dywimages/spree/images/68141/original/open-uri20171023-5-1e6ecr6.', colors: ['Orange', 'Blue']},
-            {name: 'Xylem - Sultana', sku: 'AD372-2', image_url: 'https://s3.amazonaws.com/dywimages/spree/images/68142/original/open-uri20171023-5-izj1ro.', colors: ['Blue', 'Black and White']},
-            {name: 'Xylem - Volcanic', sku: 'AD372-3', image_url: 'https://s3.amazonaws.com/dywimages/spree/images/68143/original/open-uri20171023-5-se2bo6.', colors: ['Red', 'Indigo']},
+            {name: 'Xylem - Chloroplast', sku: 'AD372-1', image_urls: ['https://s3.amazonaws.com/dywimages/spree/images/68141/original/open-uri20171023-5-1e6ecr6.'], colors: ['Orange', 'Blue']},
+            {name: 'Xylem - Sultana', sku: 'AD372-2', image_urls: ['https://s3.amazonaws.com/dywimages/spree/images/68142/original/open-uri20171023-5-izj1ro.'], colors: ['Blue', 'Black and White']},
+            {name: 'Xylem - Volcanic', sku: 'AD372-3', image_urls: ['https://s3.amazonaws.com/dywimages/spree/images/68143/original/open-uri20171023-5-se2bo6.'], colors: ['Red', 'Indigo']},
         ]
     }
   ]
@@ -41,11 +41,13 @@ class PopulateVariants < ActiveRecord::Migration
           sku: v[:sku]
         })
 
-        variant_image = VariantImage.create!({
-          remote_file_url: v[:image_url],
-          type: 'VariantImage',
-          owner_id: variant.id
-        })
+        v[:image_urls].each do |url|
+          VariantImage.create!({
+            remote_file_url: url,
+            type: 'VariantImage',
+            owner_id: variant.id
+          })
+        end
 
         variant.colors << Color.where(name: v[:colors])
 
