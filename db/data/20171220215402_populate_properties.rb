70ddde786685bbc4a3c4a4916780cf9a59ec21a4
Edwin Horneij <edwin@astekwallcovering.com>
2018-07-24 15:16:41 -0700

    Update properties

--- db/data/20171220215402_populate_properties.rb
+++ db/data/20171220215402_populate_properties.rb
@@ -1,14 +1,24 @@
 class PopulateProperties < ActiveRecord::Migration
 
   PROPERTIES = [
-      { name: 'roll width', presentation: 'Roll width', klass_scope: 'Design' },
-      { name: 'roll length', presentation: 'Roll length', klass_scope: 'Design' },
-      { name: 'repeat height', presentation: 'Repeat height', klass_scope: 'Design' },
-      { name: 'repeat length', presentation: 'Repeat length', klass_scope: 'Design' },
-      { name: 'mural width', presentation: 'Mural width', klass_scope: 'Design' },
-      { name: 'mural height', presentation: 'Mural height', klass_scope: 'Design' },
-      { name: 'match type', presentation: 'Match type', klass_scope: 'Design' },
-      { name: 'printed width', presentation: 'Printed width', klass_scope: 'Design' },
+      { name: 'roll width', presentation: 'Roll width (inches)', klass_scope: 'Design' },
+      { name: 'roll length', presentation: 'Roll length (yards)', klass_scope: 'Design' },
+      { name: 'mural width', presentation: 'Mural width (inches)', klass_scope: 'Design' },
+      { name: 'mural height', presentation: 'Mural height (inches)', klass_scope: 'Design' },
+      { name: 'motif height', presentation: 'Motif height (inches)', klass_scope: 'Design' },
+      { name: 'motif width', presentation: 'Motif width (inches)', klass_scope: 'Design' },
+      { name: 'repeat match type', presentation: 'Repeat match type', klass_scope: 'Design' },
+      { name: 'printed width', presentation: 'Printed width (inches)', klass_scope: 'Design' },
+      { name: 'sale quantity', presentation: 'Sold in quantities of', klass_scope: 'Design' },
+      { name: 'application', presentation: 'Application', klass_scope: 'Design' },
+      { name: 'removability', presentation: 'Removability', klass_scope: 'Design' },
+      { name: 'washability', presentation: 'Washability', klass_scope: 'Design' },
+      { name: 'color fastness', presentation: 'Color fastness', klass_scope: 'Design' },
+      { name: 'backing', presentation: 'Backing', klass_scope: 'Design' },
+      { name: 'fire rating', presentation: 'Fire rating', klass_scope: 'Design' },
+      { name: 'print type', presentation: 'Print type', klass_scope: 'Design' },
+      { name: 'margin trim', presentation: 'Margin trim', klass_scope: 'Design' },
+      { name: 'print to order', presentation: 'Print to order', klass_scope: 'Design' },
   ]
 
   # require 'pp'
