2da036f08538696134e9165630a1eb172ffaa1cd
Edwin Horneij <edwin@astek.com>
2018-08-02 11:40:20 -0700

    Add Paper type to backings

--- db/data/20180302230701_populate_backing_types.rb
+++ db/data/20180302230701_populate_backing_types.rb
@@ -1,6 +1,6 @@
 class PopulateBackingTypes < ActiveRecord::Migration
   def up
-    %w[Woven Non-woven].each do |t|
+    %w[Woven Non-woven Paper].each do |t|
       BackingType.create(name: t)
     end
   end
