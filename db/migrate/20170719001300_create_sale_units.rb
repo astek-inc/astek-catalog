8c77e0caa7600396be2abe2b4765ae9f519c6d6a
Edwin Horneij <edwin@astek.com>
2018-08-07 15:18:57 -0700

    Remove unused rows from sale_units table

--- db/migrate/20170719001300_create_sale_units.rb
+++ db/migrate/20170719001300_create_sale_units.rb
@@ -3,8 +3,6 @@ class CreateSaleUnits < ActiveRecord::Migration
     create_table :sale_units do |t|
       t.string :name
       t.text :description
-      t.string :slug, unique: true
-      t.integer :row_order, index: true
       t.timestamps null: false
       t.timestamp :deleted_at, index: true
     end
