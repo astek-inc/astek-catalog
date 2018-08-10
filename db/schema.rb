a3778bee35e424bd2ab49cbdf24c41863a4fefb9
Edwin Horneij <edwin@astek.com>
2018-08-10 11:41:06 -0700

    Add updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -297,6 +297,16 @@ ActiveRecord::Schema.define(version: 20180118235116) do
 
   add_index "vendors", ["deleted_at"], name: "index_vendors_on_deleted_at", using: :btree
 
+  create_table "websites", force: :cascade do |t|
+    t.string   "name"
+    t.string   "domain"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.datetime "deleted_at"
+  end
+
+  add_index "websites", ["deleted_at"], name: "index_websites_on_deleted_at", using: :btree
+
   add_foreign_key "collections", "product_categories"
   add_foreign_key "collections", "vendors"
   add_foreign_key "designs", "collections"
