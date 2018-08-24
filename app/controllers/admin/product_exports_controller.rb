ef3127eb53964ddbd406a7d4f6821d650f10d32e
Edwin Horneij <edwin@astek.com>
2018-08-24 11:05:10 -0700

    Group by design, not variant

--- app/controllers/admin/product_exports_controller.rb
+++ app/controllers/admin/product_exports_controller.rb
@@ -11,8 +11,11 @@ module Admin
       collection = Collection.find(params[:collection_id])
       website = Website.find(params[:website_id])
 
-      variants = collection.designs.map { |d| d.variants }.flatten
-      csv_data = variants_to_csv variants, website.domain
+
+      csv_data = ''
+      collection.designs.each do |design|
+        csv_data += variants_to_csv design, website.domain, csv_data.empty?
+      end
 
       respond_to do |format|
         format.csv { send_data csv_data, type: 'text/csv; charset=utf-8; header=present', filename: "#{website.name.parameterize}-product-export-#{collection.name.parameterize}-#{Date.today}.csv" }
