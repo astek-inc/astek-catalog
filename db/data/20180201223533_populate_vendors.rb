8a9ae82984c7657d41953673ec912a77d2a1b6de
Edwin Horneij <edwin@astek.com>
2018-09-05 09:56:38 -0700

    Move all vendor data to one data migration

--- db/data/20180201223533_populate_vendors.rb
+++ db/data/20180201223533_populate_vendors.rb
@@ -1,8 +1,20 @@
 class PopulateVendors < ActiveRecord::Migration
 
   VENDORS = [
+      { name: 'Anaglypta', description: '' },
+      { name: 'Asiana', description: '' },
       { name: 'Astek', description: 'In-house design and production' },
+      { name: 'BN', description: '' },
+      { name: 'Capiz Shells', description: '' },
+      { name: 'DC Fix', description: '' },
+      { name: 'Desima', description: '' },
+      { name: 'Marburg', description: '' },
       { name: 'Nina Hancock', description: '' },
+      { name: 'Omni', description: '' },
+      { name: 'Rotex', description: '' },
+      { name: 'Tokiwa', description: '' },
+      { name: 'Tokyo', description: '' },
+      { name: 'Wallquest', description: '' },
       { name: 'Wallquest', description: '' },
   ]
 
