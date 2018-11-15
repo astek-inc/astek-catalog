73c2b265e92ca04a60866ea2bfa66a7c91a2bad4
Edwin Horneij <edwin@astek.com>
2018-11-15 12:33:25 -0800

    Output Colorway columns when product can be customized, even if product has no real colorways

--- lib/admin/product_data_csv_generator.rb
+++ lib/admin/product_data_csv_generator.rb
@@ -253,7 +253,7 @@ module Admin
         when 'astek.com'
           'Colorway'
         when 'astekhome.com'
-          if variant.variant_type.name == 'Color Way'
+          if variant.variant_type.name == 'Color Way' || variant.design.digital?
            'Colorway'
           else
             'Size'
@@ -270,7 +270,7 @@ module Admin
             variant.name
           end
         when 'astekhome.com'
-          if variant.variant_type.name == 'Color Way'
+          if variant.variant_type.name == 'Color Way' || variant.design.digital?
             astek_home_colorway_value variant_type, variant.name
           else
             astek_home_size_value variant_type
@@ -327,7 +327,7 @@ module Admin
         when 'astek.com'
           nil
         when 'astekhome.com'
-          if variant.variant_type.name == 'Color Way'
+          if variant.variant_type.name == 'Color Way' || variant.design.digital?
             'Size'
           else
             if variant.design.collection.user_can_select_material
@@ -341,7 +341,7 @@ module Admin
         when 'astek.com'
           nil
         when 'astekhome.com'
-          if variant.variant_type.name == 'Color Way'
+          if variant.variant_type.name == 'Color Way' || variant.design.digital?
             astek_home_size_value variant_type
           else
             astek_home_material_value material
@@ -353,7 +353,7 @@ module Admin
         when 'astek.com'
           nil
         when 'astekhome.com'
-          if variant.variant_type.name == 'Color Way' && variant.design.collection.user_can_select_material
+          if (variant.variant_type.name == 'Color Way' || variant.design.digital?) && variant.design.collection.user_can_select_material
             'Material'
           end
         end
@@ -363,7 +363,7 @@ module Admin
         when 'astek.com'
           nil
         when 'astekhome.com'
-          if variant.variant_type.name == 'Color Way'
+          if (variant.variant_type.name == 'Color Way' || variant.design.digital?) && variant.design.collection.user_can_select_material
             astek_home_material_value material
           end
         end
