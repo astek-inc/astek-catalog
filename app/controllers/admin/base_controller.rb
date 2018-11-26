23fbcf728824f2671c24eb07d716aa544a6b7a2b
Edwin Horneij <edwin@astek.com>
2018-11-26 14:08:21 -0800

    Use different layout for frontend

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class BaseController < ApplicationController
 
+    layout 'admin/application'
 
   end
 end
--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,3 +1,3 @@
 class BaseController < ApplicationController
-
+  layout 'application'
 end
