24e197e07b0c57d03322a903277206758470630b
Edwin Horneij <edwin@astek.com>
2019-01-04 17:08:28 -0800

    Add data migration to update From Above collection pricing and substrates

---
+++ db/data/20190105010712_update_from_above_pricing_and_substrates.rb
@@ -0,0 +1,60 @@
+class UpdateFromAbovePricingAndSubstrates < ActiveRecord::Migration
+
+  DATA_FILE = '2019-01-04_from_above_pricing_and_substrates.csv'
+  DATA_DIR = 'data'
+
+  require 'csv'
+
+  def up
+
+    path = File.join(__dir__, DATA_DIR, DATA_FILE)
+    csv = CSV.read(path, headers: true)
+
+    csv.each do |row|
+
+      puts '- '*30
+
+      item = OpenStruct.new row.to_h
+
+      design = Design.find_by(sku: item.design_sku.strip)
+      unless design.nil?
+        puts design.name
+
+        variant = Variant.find_by(sku: item.sku.strip)
+        unless variant.nil?
+          puts variant.name
+
+          substrate = Substrate.find_by(name: item.substrate.strip)
+          unless substrate.nil?
+
+
+            price = BigDecimal(item.price.strip.gsub(/,/, ''), 2)
+            puts price
+
+            puts substrate.name
+
+            design.price = price
+            design.save!
+
+            variant.substrate = substrate
+            variant.save!
+
+          end
+
+        end
+      end
+
+
+
+      puts $\
+
+
+    end
+
+    # raise
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration
+  end
+end
--- db/data_schema.rb
+++ db/data_schema.rb
@@ -1,2 +1,2 @@
 # encoding: UTF-8
-DataMigrate::Data.define(version: 20190105010357)
+DataMigrate::Data.define(version: 20190105010712)
