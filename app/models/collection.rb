328b61b7599be551c6cad061a5ef8c3a462e1845
Edwin Horneij <edwin@astek.com>
2019-03-08 10:48:17 -0800

    Add default ordering for relations

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -6,7 +6,7 @@ class Collection < ActiveRecord::Base
 
   belongs_to :product_category, inverse_of: :collections
 
-  has_many :designs, dependent: :destroy
+  has_many :designs, -> { order(row_order: :asc) }, dependent: :destroy
   has_many :collection_images, -> { order(row_order: :asc) }, foreign_key: 'owner_id', dependent: :destroy
 
   belongs_to :lead_time
--- app/models/design.rb
+++ app/models/design.rb
@@ -14,7 +14,7 @@ class Design < ActiveRecord::Base
   belongs_to :sale_unit
   belongs_to :country_of_origin, class_name: 'Country', foreign_key: 'country_id'
 
-  has_many :variants, dependent: :destroy
+  has_many :variants, -> { order(row_order: :asc) }, dependent: :destroy
 
   has_many :design_images, -> { order(row_order: :asc) }, foreign_key: 'owner_id', dependent: :destroy
 
