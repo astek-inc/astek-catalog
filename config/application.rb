c25072bf0db6c22517a3487496ba6826d441bec5
Edwin Horneij <edwin@astek.com>
2019-03-20 15:30:06 -0700

    Restore an accidentally removed config setting, and some comments

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,12 @@ module AstekCatalog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    ActiveModelSerializers.config.adapter = :json_api
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
