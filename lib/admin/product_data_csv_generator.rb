b11c0bb9e4b915ce36d7b5d2f81375c5850db83f
Edwin Horneij <edwinhorneij@outlook.com>
2020-05-22 11:10:20 -0700

    Add "feet" as one of the units of length to be output

--- lib/admin/product_data_csv_generator.rb
+++ lib/admin/product_data_csv_generator.rb
@@ -812,7 +812,7 @@ module Admin
       end
 
       def format_property_value dp
-        if matches = dp.property.name.match(/_(?<unit>inches|yards|meters)\Z/)
+        if matches = dp.property.name.match(/_(?<unit>inches|feet|yards|meters)\Z/)
           "#{dp.value} #{matches[:unit]}"
         elsif dp.property.name == 'margin_trim' && !%w[Pre-trimmed Untrimmed].include?(dp.value)
           # Value for margin trim can be numeric, but we display "Untrimmed"
