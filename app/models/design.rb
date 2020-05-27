05c42832fb78268e218c1b0b43cd16308fa931d9
Edwin Horneij <edwinhorneij@outlook.com>
2020-05-27 14:45:18 -0700

    Parameterize type tag values

--- app/models/design.rb
+++ app/models/design.rb
@@ -115,7 +115,7 @@ class Design < ApplicationRecord
       tags = []
 
       tags << to_tags('style', self.styles.map { |s| s.name })
-      tags << to_tags('type', self.variants.map { |v| v.product_types.select { |t| t.websites.map { |w| w.domain }.include?(domain) }.map { |t| t.name.downcase } }.flatten.uniq)
+      tags << to_tags('type', self.variants.map { |v| v.product_types.select { |t| t.websites.map { |w| w.domain }.include?(domain) }.map { |t| t.name.parameterize } }.flatten.uniq)
 
       if domain == 'astek.com'
         if self.digital?
