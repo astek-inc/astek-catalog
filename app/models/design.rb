d36a1aed53dc99bbd8c8d08e9d986ca65c81d9ff
Edwin Horneij <edwinhorneij@outlook.com>
2021-01-08 13:52:30 -0800

    Use first variant to get minimum quantity

--- app/models/design.rb
+++ app/models/design.rb
@@ -241,21 +241,21 @@ class Design < ApplicationRecord
 
   def calculator_settings
 
-    case self.sale_unit.name
+    case self.variants.first.sale_unit.name
     when 'Roll'
       width = self.property('roll_width_inches')
       if length = self.property('roll_length_yards')
         {
             note: "Indicate no. of Rolls <span>(1 Roll = #{width} in. x #{length} yd.)</span>",
             divisor: (BigDecimal(width, 9) * (BigDecimal(length, 9) * BigDecimal('36', 9))),
-            minimum: self.minimum_quantity,
+            minimum: self.variants.first.minimum_quantity,
             sale_unit: %w[roll rolls]
         }.to_json
       elsif length = self.property('roll_length_feet')
         {
             note: "Indicate no. of Rolls <span>(1 Roll = #{width} in. x #{length} ft.)</span>",
             divisor: (BigDecimal(width, 9) * (BigDecimal(length, 9) * BigDecimal('12', 9))),
-            minimum: self.minimum_quantity,
+            minimum: self.variants.first.minimum_quantity,
             sale_unit: %w[roll rolls]
         }.to_json
       end
@@ -265,14 +265,14 @@ class Design < ApplicationRecord
       length = '36'
 
       parenthetical_note = "#{width} in. wide roll, sold per yard."
-      if self.minimum_quantity > 1
-        parenthetical_note += " This product has a minimum order quantity of #{self.minimum_quantity} yards."
+      if self.variants.first.minimum_quantity > 1
+        parenthetical_note += " This product has a minimum order quantity of #{self.variants.first.minimum_quantity} yards."
       end
 
       {
           note: "Indicate no. of Yards <span>(#{parenthetical_note})</span>",
           divisor: (BigDecimal(width, 9) * BigDecimal(length, 9)),
-          minimum: self.minimum_quantity,
+          minimum: self.variants.first.minimum_quantity,
           sale_unit: %w[yard yards]
       }.to_json
 
@@ -281,22 +281,22 @@ class Design < ApplicationRecord
       length = '39.37'
 
       parenthetical_note = "#{width} in. wide roll, sold per meter."
-      if self.minimum_quantity > 1
-        parenthetical_note += " This product has a minimum order quantity of #{self.minimum_quantity} meters."
+      if self.variants.first.minimum_quantity > 1
+        parenthetical_note += " This product has a minimum order quantity of #{self.variants.first.minimum_quantity} meters."
       end
 
       {
           note: "Indicate no. of Meters <span>(#{parenthetical_note})</span>",
           divisor: (BigDecimal(width, 9) * BigDecimal(length, 9)),
-          minimum: self.minimum_quantity,
+          minimum: self.variants.first.minimum_quantity,
           sale_unit: %w[meter meters]
       }.to_json
 
     when 'Square Foot'
       {
-          note: "Indicate no. of Square Feet<br><span>This product is custom printed. Minimum order quantity of #{self.minimum_quantity} square feet. We suggest adding an additional 20-30% overage to be sure you're covered!</span>",
+          note: "Indicate no. of Square Feet<br><span>This product is custom printed. Minimum order quantity of #{self.variants.first.minimum_quantity} square feet. We suggest adding an additional 20-30% overage to be sure you're covered!</span>",
           divisor: 144,
-          minimum: self.minimum_quantity,
+          minimum: self.variants.first.minimum_quantity,
           sale_unit: ['square foot', 'square feet']
       }.to_json
 
@@ -316,9 +316,9 @@ class Design < ApplicationRecord
       end
 
       {
-          note: "Indicate no. of Sets of #{quantity} Panels<br><span>Minimum order quantity of #{self.minimum_quantity} set. We suggest adding an additional 20-30% overage to be sure you're covered!</span>",
+          note: "Indicate no. of Sets of #{quantity} Panels<br><span>Minimum order quantity of #{self.variants.first.minimum_quantity} set. We suggest adding an additional 20-30% overage to be sure you're covered!</span>",
           divisor: divisor,
-          minimum: self.minimum_quantity,
+          minimum: self.variants.first.minimum_quantity,
           sale_unit: ['set', 'sets']
       }.to_json
 
@@ -328,7 +328,7 @@ class Design < ApplicationRecord
       {
           note: "Indicate no. of Panels<br><span>We suggest adding an additional 20-30% overage to be sure you're covered!</span>",
           divisor: (BigDecimal(width, 9) * BigDecimal(height, 9)),
-          minimum: self.minimum_quantity,
+          minimum: self.variants.first.minimum_quantity,
           sale_unit: ['panel', 'panels']
       }.to_json
     end
