a62155c351b6f7130fa0c93e05ef6694bc2ae330
Edwin Horneij <edwinhorneij@outlook.com>
2021-01-29 16:30:10 -0800

    Update data migration to actually create the stock items

--- db/data/20210122005242_populate_stock_items_from_custom_materials.rb
+++ db/data/20210122005242_populate_stock_items_from_custom_materials.rb
@@ -1,41 +1,46 @@
 class PopulateStockItemsFromCustomMaterials < ActiveRecord::Migration[5.2]
   def up
-    Design.where(user_can_select_material: true).each_with_index do |d, i|
-      # puts i
-      puts d.sku
-      puts d.name
+    Design.where(user_can_select_material: true).each_with_index do |design, i|
 
-      puts 'Variants:'
-      d.variants.each do |v|
-        puts v.sku
+      # puts '- + - ' * 20
+      puts "Design: #{design.name} - #{design.sku}"
 
-        puts 'Stock Items:'
-        v.stock_items.each do |s|
-          puts "\t"+s.substrate_or_backing
+      # puts 'Variants:'
+      design.variants.each do |variant|
+        # puts "#{variant.name} - #{variant.sku}"
 
-          puts 'Create Stock Items:'
-          d.custom_materials.each do |c|
+        # puts 'Stock Items:'
+        variant.stock_items.each do |stock_item|
 
+          # puts 'Create Stock Items:'
+          design.custom_materials.each do |custom_material|
 
+            # puts '---'
+            # puts "\t#{custom_material.substrate.name}"
 
-            puts "\t#{c.substrate.name} :: #{s.substrate.name}"
-            if s.substrate == c.substrate
-              puts "\t--Skip this one"
+            #   puts "\t--This stock item already exists. Set its row order position to first"
+            if stock_item.substrate == custom_material.substrate
+              stock_item.update row_order_position: :first
+              next
             end
 
-            puts "\tPrice: #{(BigDecimal(s.price, 0) + BigDecimal(c.surcharge, 0))}"
-            puts "\tWeight: #{c.substrate.weight_per_square_foot}"
+            # puts "\tPrice: #{(BigDecimal(stock_item.price, 0) + BigDecimal(custom_material.surcharge, 0))}"
+            # puts "\tWeight: #{custom_material.substrate.weight_per_square_foot}"
 
-            # {
-            #   price: (BigDecimal(s.price, 0) + BigDecimal(c.surcharge, 0)),
-            #   sale_unit_id: s.sale_unit_id,
-            #   sale_quantity: s.sale_quantity,
-            #   minimum_quantity: s.minimum_quantity,
-            #   weight: c.substrate.weight_per_square_foot,
-            #   width: s.width,
-            #   height: s.height,
-            #   depth: s.depth
-            # }
+            new_stock_item = StockItem.create!({
+              variant_id: variant.id,
+              substrate_id: custom_material.substrate.id,
+              price: (BigDecimal(stock_item.price, 0) + BigDecimal(custom_material.surcharge, 0)),
+              sale_unit_id: stock_item.sale_unit_id,
+              sale_quantity: stock_item.sale_quantity,
+              minimum_quantity: stock_item.minimum_quantity,
+              weight: custom_material.substrate.weight_per_square_foot,
+              width: stock_item.width,
+              height: stock_item.height,
+              depth: stock_item.depth
+            })
+
+            new_stock_item.websites = stock_item.websites
 
           end
         end
@@ -44,10 +49,10 @@ class PopulateStockItemsFromCustomMaterials < ActiveRecord::Migration[5.2]
 
       end
 
-      puts '- '*10
+      # puts '- '*10
     end
 
-    raise
+    # raise
   end
 
   def down
