856976f72c3321125f22f6f725b60631bf764721
Edwin Horneij <edwin@astek.com>
2021-03-04 10:12:26 -0800

    Update body field of data export to Shopify to change product page layout

--- lib/admin/product_data_csv_generator.rb
+++ lib/admin/product_data_csv_generator.rb
@@ -7,6 +7,24 @@ module Admin
     SAMPLE_PRICE_DIGITAL = '10.99'
     SAMPLE_PRICE_DISTRIBUTED = '5.99'
 
+    DIMENSIONAL_PROPERTIES = %w[
+      motif_width_inches
+      mural_height_inches
+      mural_width_inches
+      panel_height_inches
+      panels_per_set
+      panel_width_inches
+      printed_width_inches
+      repeat_match_type
+      roll_length_feet
+      roll_length_meters
+      roll_length_yards
+      roll_width_inches
+      tile_height_inches
+      tile_width_inches
+      vertical_repeat_inches
+    ]
+
     TEXT_VALUES = {
         variant_barcode: '',
         variant_inventory_tracker: '',
@@ -805,48 +823,12 @@ module Admin
         variant = stock_item.variant
         design = variant.design
 
-        formatted = '<div class="description__meta">'
+        formatted = '<!-- DESCRIPTION V2 -->
+        <div class="description__meta">'
 
-        formatted += '<div>
-              <h6>SKU</h6>
-              <p>'+design.sku+'</p>
-            </div>'
-
-        unless design.collection.suppress_from_display
-          formatted += '<div>
-              <h6>Collection</h6>
-              <p><a href="/collections/'+design.collection.name.parameterize+'">'+design.collection.name+'</a></p>
-            </div>'
-        end
-
-        if design.collection.lead_time
-          formatted += '<div>
-              <h6>Lead Time</h6>
-              <p>'+design.collection.lead_time.name+'</p>
-            </div>'
-        end
-
-        design.design_properties.each do |dp|
-          next if /\Aroll_length_/ =~ dp.property.name && stock_item.sale_unit.name != 'Roll'
-          formatted += '<div>
-            <h6>'+dp.property.presentation+'</h6>
-            <p>'+format_property_value(dp, 'astekhome.com')+'</p>
-          </div>'
-        end
-
-        if stock_item.minimum_quantity > 1
-          formatted += '<div>
-            <h6>Minimum quantity</h6>
-            <p>'+stock_item.minimum_quantity.to_s+' '+stock_item.sale_unit.name.pluralize.titleize+'</p>
-          </div>'
-        end
-
-        if stock_item.sale_quantity > 1
-          formatted += '<div>
-            <h6>Sold in quantities of</h6>
-            <p>'+stock_item.sale_quantity.to_s+'</p>
-          </div>'
-        end
+        formatted += format_dimensional_properties design, stock_item
+        formatted += format_shipping_and_returns_information
+        formatted += format_additional_specs design, stock_item
 
         formatted += '</div>'
 
@@ -863,6 +845,112 @@ module Admin
         end
 
         formatted
+      end
+
+      def format_dimensional_properties design, stock_item
+
+        items = ''
+
+        design.design_properties.each do |dp|
+          if DIMENSIONAL_PROPERTIES.include? dp.property.name
+
+            next if /\Aroll_length_/ =~ dp.property.name && stock_item.sale_unit.name != 'Roll'
+            items += '<div>
+              <dt>'+dp.property.presentation+'<dt>
+              <dd>'+format_property_value(dp, 'astekhome.com')+'</dd>
+            </div>'
+
+          end
+        end
+
+        return '<div class="dropdown">
+          <div class="dropdown-header">
+            <span>Size + Repeat</span> <span class="dropdown-caret down"></span>
+          </div>
+
+          <div class="dropdown-body">
+            <dl>
+              ' + items + '
+            </dl>
+          </div>
+        </div>'
+
+      end
+
+      def format_shipping_and_returns_information
+
+        return '<div class="dropdown">
+          <div class="dropdown-header">
+            <span>Shipping + Returns</span> <span class="dropdown-caret down"></span>
+          </div>
+
+          <div class="dropdown-body">
+            <p>This product is digitally printed to order. We do not accept returns or exchanges on digitally printed products.</p>
+            <p>Digitally printed products require 1-2 weeks to print before being shipped.</p>
+            <p>To learn more, visit our <a href="/pages/shipping-policy">shipping policy and returns page</a>.</p>
+          </div>
+        </div>'
+
+      end
+
+      def format_additional_specs design, stock_item
+
+        items = ''
+
+        items += '<div>
+          <dt>SKU</dt>
+          <dd>'+design.sku+'</dd>
+        </div>'
+
+        unless design.collection.suppress_from_display
+          items += '<div>
+            <dt>Collection</dt>
+            <dd><a href="/collections/'+design.collection.name.parameterize+'">'+design.collection.name+'</a></dd>
+          </div>'
+        end
+
+        if design.collection.lead_time
+          items += '<div>
+            <dt>Lead time</dt>
+            <dd>'+design.collection.lead_time.name+'</dd>
+          </div>'
+        end
+
+        design.design_properties.each do |dp|
+          if DIMENSIONAL_PROPERTIES.exclude? dp.property.name
+            items += '<div>
+              <dt>'+dp.property.presentation+'</dt>
+              <dd>'+format_property_value(dp, 'astekhome.com')+'</dd>
+            </div>'
+          end
+        end
+
+        if stock_item.minimum_quantity > 1
+          items += '<div>
+            <dt>Minimum quantity</dt>
+            <dd>'+stock_item.minimum_quantity.to_s+' '+stock_item.sale_unit.name.pluralize.titleize+'</dd>
+          </div>'
+        end
+
+        if stock_item.sale_quantity > 1
+          items += '<div>
+            <dt>Sold in quantities of</dt>
+            <dd>'+stock_item.sale_quantity.to_s+'</dd>
+          </div>'
+        end
+
+        return '<div class="dropdown">
+          <div class="dropdown-header">
+            <span>Additional Specs</span> <span class="dropdown-caret down"></span>
+          </div>
+
+          <div class="dropdown-body">
+            <dl>
+              ' + items + '
+            </dl>
+          </div>
+        </div>'
+
       end
 
       def format_onair_properties stock_item
